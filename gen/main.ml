8d78265a55e6af5b885b7c5ba8dfd178475493e5
patricoferris <pf341@patricoferris.com>
2021-01-15 16:29:39 +0000

    test...

--- gen/main.ml
+++ gen/main.ml
@@ -1,4 +1,3 @@
-open Sexplib
 module Sysp = OpamSysPoll
 
 let get err = function Some t -> t | None -> failwith err
@@ -11,16 +10,14 @@ let write () =
   in
   let os_version = Sysp.os_version () |> get "failed getting version" in
   let os_family = Sysp.os_family () |> get "failed getting family" in
-  let v : Osinfo.t = { arch; os; os_family; os_distribution; os_version } in
-  let sexp = Osinfo.sexp_of_t v in
-  Sexp.pp Format.std_formatter sexp
+  { Osinfo.arch; os; os_family; os_distribution; os_version }
 
 let format s =
   let ocamlformat = Bos.Cmd.(v "ocamlformat" % "--impl" % "-") in
   Bos.OS.Cmd.(in_string s |> run_io ocamlformat |> out_string)
   |> Rresult.R.get_ok |> fst
 
-let read f =
+let run v =
   let module_name = String.capitalize_ascii in
   let v_to_string v = String.split_on_char '.' v |> String.concat "_" in
   let pp ppf v =
@@ -28,10 +25,6 @@ let read f =
     let x = Fmt.str "module %s = struct\n let v = %a\nend" s Osinfo.pp v in
     Fmt.(pf ppf "%s" (format x))
   in
-  match Bos.OS.File.read (Fpath.v f) with
-  | Ok s ->
-      let v = Osinfo.t_of_sexp (Sexp.of_string s) in
-      pp Format.std_formatter v
-  | Error (`Msg m) -> failwith m
+  pp Format.std_formatter v
 
-let () = if Array.length Sys.argv <= 1 then write () else read Sys.argv.(2)
+let () = run (write ())
