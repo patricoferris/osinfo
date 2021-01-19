1b66674895b871bda17992e8cdccbcfc75c05d43
patricoferris <patricoferris@users.noreply.github.com>
2021-01-19 14:52:09 +0000

    update os variables

---
+++ src/main.ml
@@ -0,0 +1,80 @@
+module Linux3_12_3 = struct
+  let v =
+    {
+      arch = "x86_64";
+      os = "linux";
+      os_distribution = "alpine";
+      os_version = "3.12.3";
+      os_family = "alpine";
+    }
+end
+module Linux8 = struct
+  let v =
+    {
+      arch = "x86_64";
+      os = "linux";
+      os_distribution = "centos";
+      os_version = "8";
+      os_family = "rhel";
+    }
+end
+module Linux10 = struct
+  let v =
+    {
+      arch = "x86_64";
+      os = "linux";
+      os_distribution = "debian";
+      os_version = "10";
+      os_family = "debian";
+    }
+end
+module Linux32 = struct
+  let v =
+    {
+      arch = "x86_64";
+      os = "linux";
+      os_distribution = "fedora";
+      os_version = "32";
+      os_family = "fedora";
+    }
+end
+module Macos10_15_7 = struct
+  let v =
+    {
+      arch = "x86_64";
+      os = "macos";
+      os_distribution = "homebrew";
+      os_version = "10.15.7";
+      os_family = "homebrew";
+    }
+end
+module Linux15_2 = struct
+  let v =
+    {
+      arch = "x86_64";
+      os = "linux";
+      os_distribution = "opensuse-leap";
+      os_version = "15.2";
+      os_family = "suse";
+    }
+end
+module Linux18_04 = struct
+  let v =
+    {
+      arch = "x86_64";
+      os = "linux";
+      os_distribution = "ubuntu";
+      os_version = "18.04";
+      os_family = "debian";
+    }
+end
+module Win3210_0_17763 = struct
+  let v =
+    {
+      arch = "x86_64";
+      os = "win32";
+      os_distribution = "win32";
+      os_version = "10.0.17763";
+      os_family = "windows";
+    }
+end
