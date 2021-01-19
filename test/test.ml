2df64410fa1e27462f179ce64bd2b115ed57369f
patricoferris <pf341@patricoferris.com>
2021-01-19 18:42:05 +0000

    run tests

---
+++ test/test.ml
@@ -0,0 +1,45 @@
+open Osinfo
+
+let unixes =
+  [
+    ("Ubuntu", Os.Ubuntu18_04.v);
+    ("MacOS", Os.Homebrew10_15_7.v);
+    ("Alpine", Os.Alpine3_12_3.v);
+    ("Debian", Os.Debian10.v);
+    ("Centos", Os.Centos8.v);
+    ("Fedora", Os.Fedora32.v);
+  ]
+
+let env t =
+  Opam_0install.Dir_context.std_env ~arch:t.Types.arch ~os:t.os
+    ~os_family:t.os_family ~os_distribution:t.os_distribution
+    ~os_version:t.os_version ()
+
+let unix_context (name, v) =
+  ( name,
+    Opam_0install.Dir_context.create "/tmp/opam-repository/packages"
+      ~constraints:OpamPackage.Name.Map.empty ~env:(env v) )
+
+let windows_context =
+  ( "windows",
+    Opam_0install.Dir_context.create "/tmp/opam-repository-mingw/packages"
+      ~constraints:OpamPackage.Name.Map.empty ~env:(env Os.Win3210_0_17763.v) )
+
+module Solver = Opam_0install.Solver.Make (Opam_0install.Dir_context)
+
+let test_packages = [ "yaml" ]
+
+let () =
+  let build (name, ctx) =
+    let result =
+      Solver.solve ctx (List.map OpamPackage.Name.of_string test_packages)
+    in
+    match result with
+    | Error e -> print_endline (Solver.diagnostics e)
+    | Ok selections ->
+        Solver.packages_of_result selections
+        |> List.iter (fun pkg ->
+               Printf.printf "%s - %s\n" name (OpamPackage.to_string pkg))
+  in
+  List.iter build (List.map unix_context unixes);
+  build windows_context
